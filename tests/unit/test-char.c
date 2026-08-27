qemu/qemu

commit 5c102ac9f18b24bf1552bd43ca4864b4ce331a3f

chardev: Consolidate yank registration
There's currently five places where the yank function is being
registered and they all come right before tcp_chr_new_client(). Fold
them into it.

Signed-off-by: Fabiano Rosas <farosas@suse.de>
Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
Message-id: 20260302092225.4088227-8-peter.maydell@linaro.org
[PMM: rebased]
Signed-off-by: Peter Maydell <peter.maydell@linaro.org>

chardev/char-socket.c
@@ -905,6 +905,12 @@ static int tcp_chr_new_client(Chardev *chr, QIOChannelSocket *sioc)
     s->sioc = sioc;
     object_ref(OBJECT(sioc));
 
+    if (s->registered_yank) {
+        yank_register_function(CHARDEV_YANK_INSTANCE(chr->label),
+                               char_socket_yank_iochannel,
+                               QIO_CHANNEL(sioc));
+    }
+
     if (s->do_nodelay) {
         qio_channel_set_delay(s->ioc, false);
     }
@@ -943,11 +949,6 @@ static int tcp_chr_add_client(Chardev *chr, int fd)
     }
     tcp_chr_change_state(s, TCP_CHARDEV_STATE_CONNECTING);
     tcp_chr_set_client_ioc_name(chr, sioc);
-    if (s->registered_yank) {
-        yank_register_function(CHARDEV_YANK_INSTANCE(chr->label),
-                               char_socket_yank_iochannel,
-                               QIO_CHANNEL(sioc));
-    }
     ret = tcp_chr_new_client(chr, sioc);
     object_unref(OBJECT(sioc));
     return ret;
@@ -962,11 +963,6 @@ static void tcp_chr_accept(QIONetListener *listener,
 
     tcp_chr_change_state(s, TCP_CHARDEV_STATE_CONNECTING);
     tcp_chr_set_client_ioc_name(chr, cioc);
-    if (s->registered_yank) {
-        yank_register_function(CHARDEV_YANK_INSTANCE(chr->label),
-                               char_socket_yank_iochannel,
-                               QIO_CHANNEL(cioc));
-    }
     tcp_chr_new_client(chr, cioc);
 }
 
@@ -981,11 +977,6 @@ static int tcp_chr_connect_client_sync(Chardev *chr, Error **errp)
         tcp_chr_change_state(s, TCP_CHARDEV_STATE_DISCONNECTED);
         object_unref(OBJECT(sioc));
         return -1;
-    }
-    if (s->registered_yank) {
-        yank_register_function(CHARDEV_YANK_INSTANCE(chr->label),
-                               char_socket_yank_iochannel,
-                               QIO_CHANNEL(sioc));
     }
     tcp_chr_new_client(chr, sioc);
     object_unref(OBJECT(sioc));
@@ -1002,11 +993,6 @@ static void tcp_chr_accept_server_sync(Chardev *chr)
     tcp_chr_change_state(s, TCP_CHARDEV_STATE_CONNECTING);
     sioc = qio_net_listener_wait_client(s->listener);
     tcp_chr_set_client_ioc_name(chr, sioc);
-    if (s->registered_yank) {
-        yank_register_function(CHARDEV_YANK_INSTANCE(chr->label),
-                               char_socket_yank_iochannel,
-                               QIO_CHANNEL(sioc));
-    }
     tcp_chr_new_client(chr, sioc);
     object_unref(OBJECT(sioc));
 }
@@ -1180,11 +1166,6 @@ static void tcp_chr_connect_client_async(Chardev *chr)
     tcp_chr_change_state(s, TCP_CHARDEV_STATE_CONNECTING);
     sioc = qio_channel_socket_new();
     tcp_chr_set_client_ioc_name(chr, sioc);
-    if (s->registered_yank) {
-        yank_register_function(CHARDEV_YANK_INSTANCE(chr->label),
-                               char_socket_yank_iochannel,
-                               QIO_CHANNEL(sioc));
-    }
     /*
      * Normally code would use the qio_channel_socket_connect_async
      * method which uses a QIOTask + qio_task_set_error internally

kmehltretter82/qemu-sam9x75

commit 569ca3a66d766931a8b8131b6f8eb9d09edd7ce5

chardev: Avoid unregistering yank after failed reconnect
Yank callbacks are now registered by tcp_chr_new_client(), after a socket connection succeeds.  A failed asynchronous reconnect therefore has no callback associated with its temporary QIOChannelSocket.

The error path still tried to unregister that callback whenever the chardev yank instance was enabled.  After a prior live connection this makes yank_unregister_function() abort because the new socket was never registered.

Drop the stale unregister and cover failed reconnects after a live TCP or Unix connection.

Fixes: 5c102ac9f1 ("chardev: Consolidate yank registration")

chardev/char-socket.c
@@ -1128,11 +1128,6 @@ static void qemu_chr_socket_connected(QIOTask *task, void *opaque)
 
     if (qio_task_propagate_error(task, &err)) {
         tcp_chr_change_state(s, TCP_CHARDEV_STATE_DISCONNECTED);
-        if (s->registered_yank) {
-            yank_unregister_function(CHARDEV_YANK_INSTANCE(chr->label),
-                                     char_socket_yank_iochannel,
-                                     QIO_CHANNEL(sioc));
-        }
         check_report_connect_error(chr, err);
         goto cleanup;
     }

tests/unit/test-char.c
@@ -8,6 +8,7 @@
 #include "qemu/sockets.h"
 #include "chardev/char-fe.h"
 #include "chardev/char-serial.h"
+#include "chardev/char-socket.h"
 #include "system/system.h"
 #include "qapi/error.h"
 #include "qapi/qapi-commands-char.h"
@@ -1506,6 +1507,78 @@ static void char_socket_client_test(gconstpointer opaque)
     g_free(optstr);
 }
 
+static void char_socket_stop_main_loop(void *opaque)
+{
+    quit = true;
+}
+
+static void char_socket_client_failed_reconnect_test(gconstpointer opaque)
+{
+    SocketAddress *listen_addr = (gpointer)opaque;
+    QIOChannelSocket *listener = qio_channel_socket_new();
+    QIOChannelSocket *server;
+    SocketAddress *addr;
+    g_autofree char *optstr = NULL;
+    QemuOpts *opts;
+    Chardev *chr;
+    SocketChardev *socket_chr;
+    CharFrontend frontend = { 0 };
+    CharSocketTestData data = { .event = -1 };
+    QEMUTimer *stop_timer;
+
+    g_setenv("QTEST_SILENT_ERRORS", "1", 1);
+    qio_channel_socket_listen_sync(listener, listen_addr, 1, &error_abort);
+    addr = qio_channel_socket_get_local_address(listener, &error_abort);
+    g_assert_nonnull(addr);
+    optstr = char_socket_addr_to_opt_str(addr, false,
+                                         ",reconnect-ms=1", false);
+    opts = qemu_opts_parse_noisily(qemu_find_opts("chardev"),
+                                   optstr, true);
+    g_assert_nonnull(opts);
+    chr = qemu_chr_new_from_opts(opts, NULL, &error_abort);
+    qemu_opts_del(opts);
+    g_assert_nonnull(chr);
+    socket_chr = SOCKET_CHARDEV(chr);
+
+    data.fe = &frontend;
+    qemu_chr_fe_init(&frontend, chr, &error_abort);
+    qemu_chr_fe_set_handlers(&frontend, char_socket_can_read,
+                             char_socket_read, char_socket_event,
+                             NULL, &data, NULL, true);
+    server = qio_channel_socket_accept(listener, &error_abort);
+    g_assert_nonnull(server);
+    qemu_chr_wait_connected(chr, &error_abort);
+    g_assert_true(object_property_get_bool(OBJECT(chr), "connected",
+                                           &error_abort));
+    g_assert_cmpint(data.event, ==, CHR_EVENT_OPENED);
+    data.event = -1;
+
+    /* Make every automatic reconnect fail after this live peer vanishes. */
+    qio_channel_shutdown(QIO_CHANNEL(listener),
+                         QIO_CHANNEL_SHUTDOWN_BOTH, &error_abort);
+    object_unref(OBJECT(listener));
+    qio_channel_shutdown(QIO_CHANNEL(server),
+                         QIO_CHANNEL_SHUTDOWN_BOTH, &error_abort);
+    object_unref(OBJECT(server));
+
+    stop_timer = timer_new_ms(QEMU_CLOCK_REALTIME,
+                              char_socket_stop_main_loop, NULL);
+    timer_mod(stop_timer, qemu_clock_get_ms(QEMU_CLOCK_REALTIME) + 10000);
+    quit = false;
+    while (!socket_chr->connect_err_reported && !quit) {
+        main_loop_wait(false);
+    }
+    timer_del(stop_timer);
+    timer_free(stop_timer);
+
+    g_assert_true(socket_chr->connect_err_reported);
+    g_assert_false(object_property_get_bool(OBJECT(chr), "connected",
+                                            &error_abort));
+    qemu_chr_fe_deinit(&frontend, true);
+    qapi_free_SocketAddress(addr);
+    g_unsetenv("QTEST_SILENT_ERRORS");
+}
+
 static void
 count_closed_event(void *opaque, QEMUChrEvent event)
 {
@@ -2000,10 +2073,16 @@ int main(int argc, char **argv)
     if (has_ipv4) {
         SOCKET_SERVER_TEST(tcp, &tcpaddr);
         SOCKET_CLIENT_TEST(tcp, &tcpaddr);
+        g_test_add_data_func(
+            "/char/socket/client/failed-reconnect-no-yank/tcp",
+            &tcpaddr, char_socket_client_failed_reconnect_test);
     }
 #ifndef WIN32
     SOCKET_SERVER_TEST(unix, &unixaddr);
     SOCKET_CLIENT_TEST(unix, &unixaddr);
+    g_test_add_data_func(
+        "/char/socket/client/failed-reconnect-no-yank/unix",
+        &unixaddr, char_socket_client_failed_reconnect_test);
 #endif
 
     g_test_add_func("/char/udp", char_udp_test);